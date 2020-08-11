ee3a8f303fb553a9c1b8243f838525f1dd43cda1
Shashank Agarwal <63952954+shashank80-0@users.noreply.github.com>
2020-08-11 18:06:03 +0530

    Change delete order

--- Memories/application.py
+++ Memories/application.py
@@ -149,8 +149,8 @@ def deleteStory():
         storyId = request.form['story-id']
         deleteStory = story.query.filter_by(story_id = storyId).first()
         deleteOwner = owner.query.filter_by(story_id = storyId).first()
-        db.session.delete(deleteStory)
         db.session.delete(deleteOwner)
+        db.session.delete(deleteStory)
         db.session.commit()
         return redirect(url_for('home'))
 
